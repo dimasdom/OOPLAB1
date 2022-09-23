a927793dde33c4f5a709ceb6909b62366c406270
dmitry.bartash <dmitry.bartash@tesseris.com>
2022-09-23 20:39:05 +0300

    adding core

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OOPLAB1
 {
@@ -7,6 +9,49 @@ namespace OOPLAB1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        }
+        class Factory
+        {
+            public main()
+            {
+                Console.WriteLine();
+                switch (Console.ReadLine())
+                {
+                    case:;
+                        break;
+                            default
+                }
+            }
+            private List<EnergyConsuption> EnergyConsuption { get; set; }
+            public void addEnergyConsuption(double plan, double fact)
+            {
+                this.EnergyConsuption.Add(new EnergyConsuption(plan, fact));
+            }
+
+            private double getDeviation(EnergyConsuption energyConsuption)
+            {
+                return energyConsuption.plan - energyConsuption.fact;
+            }
+            private double getDeviationRate(EnergyConsuption energyConsuption)
+            {
+                return (energyConsuption.plan - energyConsuption.fact) * 100 / energyConsuption.plan;
+            }
+            (double plan, double fact) getSumResults()
+            {
+                return ((from consumption in this.EnergyConsuption select consumption.plan).Sum(), (from consumption in this.EnergyConsuption select consumption.fact).Sum());
+            }
+        }
+        class EnergyConsuption
+        {
+            public EnergyConsuption(double plan, double fact)
+            {
+                this.plan = plan;
+                this.fact = fact;
+            }
+
+            public double plan { get; set; }
+            public double fact { get; set; }
         }
     }
 }
