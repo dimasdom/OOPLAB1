788a2f264935b02e9d28b21e81ebe0b601e7d600
dmitry.bartash <dmitry.bartash@tesseris.com>
2022-09-24 13:05:24 +0300

    ui created

--- Program.cs
+++ Program.cs
@@ -13,14 +13,114 @@ namespace OOPLAB1
         }
         class Factory
         {
-            public main()
+            public void main()
             {
-                Console.WriteLine();
-                switch (Console.ReadLine())
+                Console.WriteLine("Welcome");
+                Console.WriteLine("Choose what you want to get");
+                Console.WriteLine("Type 1 for set energy consumption data");
+                Console.WriteLine("Type 2 for get deviation");
+                Console.WriteLine("Type 3 for get deviation rate");
+                Console.WriteLine("Type 4 for get deviation rate total sum");
+                Console.WriteLine("Type 5 for get all data in table");
+                string variant = Console.ReadLine();
+                switch (variant)
                 {
-                    case:;
+                    case "1":
+                        {
+                            Console.WriteLine("Set count of records");
+                            string count = Console.ReadLine();
+                            Console.WriteLine("First is plan second is fact consumtion");
+                            for(int i = 0; i < Int32.Parse(count); i++)
+                            {
+                                string data = Console.ReadLine();
+                                string[] dataArr=new string[] {};
+                                if (data.Contains(","))
+                                {
+                                    dataArr = data.Split(",");
+                                }
+                                else if (data.Contains(" "))
+                                {
+                                    dataArr = data.Split(" ");
+                                }
+                                if(dataArr.Length == 2)
+                                {
+                                    this.addEnergyConsuption(double.Parse(dataArr[0]), double.Parse(dataArr[1]));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Wrong data try again");
+                                    i -= 1;
+                                }
+                            }
+                        }
                         break;
-                            default
+                        case "2":
+                        {
+                            string data = Console.ReadLine();
+                            string[] dataArr = new string[] { };
+                            if (data.Contains(","))
+                            {
+                                dataArr = data.Split(",");
+                            }
+                            else if (data.Contains(" "))
+                            {
+                                dataArr = data.Split(" ");
+                            }
+                            if (dataArr.Length == 2)
+                            {
+                                this.getDeviation(new EnergyConsuption ( double.Parse(dataArr[0]), double.Parse(dataArr[1])));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong data try again");
+                            }
+                        }
+                        break;
+                    case "3":
+                        {
+                            string data = Console.ReadLine();
+                            string[] dataArr = new string[] { };
+                            if (data.Contains(","))
+                            {
+                                dataArr = data.Split(",");
+                            }
+                            else if (data.Contains(" "))
+                            {
+                                dataArr = data.Split(" ");
+                            }
+                            if (dataArr.Length == 2)
+                            {
+                                this.getDeviationRate(new EnergyConsuption(double.Parse(dataArr[0]), double.Parse(dataArr[1])));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong data try again");
+                            }
+                        }
+                        break;
+                    case "4":
+                        {
+                            this.getSumResults();
+                        }
+                        break;
+                    case "5":
+                        {
+                            Console.WriteLine("_______________________________________________________");
+                            Console.WriteLine("|Energy consumption kwt*h|    Deviation from plan     |");
+                            Console.WriteLine("|________________________|____________________________|");
+                            Console.WriteLine("|   planned |     fact   |    in kwt*h  |    in%      |");
+                            Console.WriteLine("|________________________|____________________________|");
+                            this.EnergyConsuption.ForEach(e =>
+                            {
+
+                                Console.WriteLine($"|   {e.plan} |     {e.fact}   |    {this.getDeviation(e)}  |    {this.getDeviationRate(e)}      |");
+                            });
+                            Console.WriteLine("|________________________|____________________________|");
+                            var sum = this.getSumResults();
+                            Console.WriteLine($"|{sum.plan} |{sum.fact}|                            |");
+                            Console.WriteLine("|________________________|____________________________|");
+                        }
+                        break;
                 }
             }
             private List<EnergyConsuption> EnergyConsuption { get; set; }
